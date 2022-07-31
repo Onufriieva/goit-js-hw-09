f6cac7c04fb4055ff2848453efc19fee787913d4
Onufriieva <onufrieva.julia@gmail.com>
2022-08-01 00:41:35 +0300

    1 завдання 1 варіант

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
 
+
+const TIME_INTERVAL = 1000;
+
+
+btnStart.addEventListener('click', (onBtnStartClick));
+btnStop.addEventListener('click', (onBtnStopClick));
+
+let intervalId = null;
+
+function onBtnStartClick() {
+  btnStart.disabled = true;
+
+intervalId = setInterval(() => {  
+  body.style.backgroundColor = getRandomHexColor();
+}, TIME_INTERVAL);
+}
+
+
+function onBtnStopClick() {
+  btnStart.disabled = false;
+clearInterval(intervalId);
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
